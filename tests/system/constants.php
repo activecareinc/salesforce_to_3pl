495180510524ef0c05210bcf8325c925aaf8d8ff
Claro Zamesa <claro.zamesa@bridgetechnologypartners.com>
2017-01-25 19:38:03 -0600

    #39194 fixes for tests

--- tests/system/constants.php
+++ tests/system/constants.php
@@ -1,6 +1,6 @@
 <?php
 // BTP environment configuration
-require_once('/srv/www/conf/proj266_constants.php');
+require_once('/srv/www/conf/proj376_constants.php');
 
 // codeigniter constants
 require_once(PROJECT_DIR_PATH . 'php_includes/application/config/constants.php');

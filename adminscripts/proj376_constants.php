83fd66b0a37b2cdd914de2cb15b371273c8027dc
Claro Zamesa <claro.zamesa@bridgetechnologypartners.com>
2017-05-29 11:12:14 +0800

    added the constant skeleton

---
+++ adminscripts/proj376_constants.php
@@ -0,0 +1,41 @@
+<?php
+define('BASE_URL', 'http://APPLICATION-URL');
+define('SITE_EMAIL', '');
+
+// API Client Key & Client ID
+define('CLIENT_ID', '7bdc6ba64a8ef52410800823070204c1986dd398c50105e9');
+define('CLIENT_SECRET', '92f97b9ac10362b09671ea9d45f3dd19c44acfbc9822cbf0');
+
+// phpunit specific constants
+define('VIEW_DOMAIN_PROTOCOL', 'http://');
+define('VIEW_DOMAIN', 'APPLICATION-DOMAIN');
+define('VIEW_ENVIRONMENT', 'remote');
+define('PROJECT_DIR_PATH', '/srv/www/proj376/');
+define('COVERAGE_SCRIPT', 'http://APPLICATION-URL/phpunit_coverage.php');
+
+// imap configuration
+define('IMAP_SERVER', '');
+define('IMAP_PORT', '');
+define('MAIL_RECIPIENT', '');
+define('MAIL_RECIPIENT_PASSWORD', '');
+
+// FACEBOOK
+define('FACEBOOK_API_KEY', '');
+define('FACEBOOK_APP_NAME', '');
+define('FACEBOOK_SECRET', '');
+define('FACEBOOK_EMAIL', '');
+define('FACEBOOK_ID', '');
+define('FACEBOOK_PASSWORD', '');
+
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'proj376');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'proj376');
+
+define('SALESFORCE_USERNAME', 'mike@agilescs.com');
+define('SALESFORCE_PASSWORD', '*btp123$');
+define('SALESFORCE_TOKEN', 'mNeeWPIMrUSdepMBu3lkHPxyF');
+
+
+define('CLIENT_ID', 'fa69cb3d-0f23-4cbc-8eea-309a5d9e7d4f');
+define('CLIENT_SECRET', 'WTmSscC3DZP+S6VEVz5KOpc/5qbDPagk');
